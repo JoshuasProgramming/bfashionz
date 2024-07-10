3914b25ac9b6ffdb18413d76fb7503fa320b4d4b
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2024-07-10 14:55:56 +0100

    Made folders for development
    components, pages and styles

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div>
+        {/* left */}
+        
+
+        {/* Right */}
+    </div>
+  )
+}
+
+export default Navigation
