6e440d2888fcffe818ff8f5cd14d3124706c7cc0
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2024-07-10 16:45:09 +0100

    Started navigation

---
+++ src/components/Hamburger.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hamburger() {
+  return (
+    <div className='hamburger'>
+        
+    </div>
+  )
+}
+
+export default Hamburger
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import styles from '../styles/style.css';
 
 function Navigation() {
   return (
-    <div>
+    <div className='navigation'>
         {/* left */}
-        
+        <div className='left-navigation'>
+            <h1>BFashionz</h1>
+        </div>
 
         {/* Right */}
+        <div className='right-navigation'>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     </div>
   )
 }
